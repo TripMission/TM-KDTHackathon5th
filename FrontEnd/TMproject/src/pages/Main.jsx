315e1e0a10fc5bc5afdf527ecebf61b91d6904b4
SUNGWHANYOON <ysh0214727@naver.com>
2023-10-24 22:06:17 +0900

    지역을 선택해주세요! 작성

--- FrontEnd/TMproject/src/pages/Main.jsx
+++ FrontEnd/TMproject/src/pages/Main.jsx
@@ -4,9 +4,15 @@ import './Main.css';
 class Main extends React.Component {
   render() {
     return (
-        <div className = 'main'>
+        <div className = 'wrap'>
            <div className = 'layout'>
-            
+              <h1>Main Page</h1>
+           </div>
+           <div className = 'main'>
+              <div className = 'mainPanel'>
+                <div className = 'geoSelect'>지역을 선택해주세요</div>
+                <div className = 'geoRecommendSelect'>추천받고 싶은 지역을 선택하세요</div>
+              </div>
            </div>
         </div>
     );
