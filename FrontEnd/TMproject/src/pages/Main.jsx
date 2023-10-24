aceebb0569fdcdc57950c11651499d358ee164cf
SUNGWHANYOON <ysh0214727@naver.com>
2023-10-24 21:30:41 +0900

    Main page Init

---
+++ FrontEnd/TMproject/src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+  render() {
+    return (
+        <div className = 'main'>
+           <div className = 'layout'>
+            
+           </div>
+        </div>
+    );
+  }
+}
+
+export default Main;
