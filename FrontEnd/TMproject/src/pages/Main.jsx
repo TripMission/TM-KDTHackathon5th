af8864b09d4428e0c3af491dbef6818e5ef5b64b
SUNGWHANYOON <ysh0214727@naver.com>
2023-10-26 22:45:13 +0900

    commit test

--- FrontEnd/TMproject/src/pages/Main.jsx
+++ FrontEnd/TMproject/src/pages/Main.jsx
@@ -9,7 +9,7 @@ class Main extends React.Component {
               <h1>Main Page</h1>
            </div>
            <div className = 'main'>
-            <div className = 'geoSelect'>지역을 선택해주세용</div>
+            <div className = 'geoSelect'>지역을 선택해주세용???</div>
             <div className = 'geoRecommendSelect'>추천받고 싶은 지역을 선택하세요</div>
             <div className = 'mainPanel'>
               <div className = 'Panel'></div>
