96eb52ac6d160f972dc88b561ba57681ee31f1f7
ysh0214727 <ysh0214727@naver.com>
2023-11-02 16:04:56 +0900

    위도 경도 출력

--- FrontEnd/TMproject/src/components/GeolocationConst.js
+++ FrontEnd/TMproject/src/components/GeolocationConst.js
@@ -1,15 +1,7 @@
 export const GEOLOCATIONOPTIONS = {
     enableHighAccuracy: true,
-    timeout: 5000,
+    timeout: 1000,
     maximumAge: 0,
   };
   
-export default function geolocationSuccess(pos) {
-    const crd = pos.coords;
-  
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-}
 
--- FrontEnd/TMproject/src/pages/Main.jsx
+++ FrontEnd/TMproject/src/pages/Main.jsx
@@ -5,33 +5,62 @@ import Header from '../components/Header'
 import RegionButton from '../components/RegionButton.jsx'
 import Layout from "../components/Layout";
 import { GEOLOCATIONOPTIONS } from '../components/GeolocationConst.js';
-import geolocationSuccess from '../components/GeolocationConst.js';
 
 class Main extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      nowLocation: null,
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      this.geolocationSuccess,
+      this.geolocationError,
+      GEOLOCATIONOPTIONS
+    );
+  }
+
+  geolocationSuccess = (pos) => {
+    const crd = pos.coords;
+    this.setState({
+      nowLocation: crd,
+      isLoading: false
+    });
+  }
+
+  geolocationError = (err) => {
+    console.error(err);
+    this.setState({
+      isLoading: false
+    });
+  }
+
   render() {
+    const { nowLocation, isLoading } = this.state;
     
-    function error(err) {
-      console.log(err)
-    }
-
-    navigator.geolocation.getCurrentPosition(geolocationSuccess, error, GEOLOCATIONOPTIONS);
-
     return (
-        <div className = 'wrap'>
-          <div>
-          </div>
-          <Layout title="메인 페이지" hasBackButton>
-           <div className = 'main'>
-            <div style={{fontFamily: '"Noto Sans KR", sans-serif'}} className = 'geoSelect'>지역을 선택해주세요</div>
-            <div style={{fontFamily: '"Noto Sans KR", sans-serif'}} className = 'geoRecommendSelect'>추천받고 싶은 지역을 선택하세요</div>
-            <div className = 'mainPanel'>
-              <RegionButton></RegionButton>
-            </div>
-           </div>
-           <Footer hasFooter={true} />
-           </Layout>
-
+      <div className='wrap'>
+        <div>
         </div>
+        <Layout title="메인 페이지" hasBackButton>
+          {isLoading && <div>Loading...</div>}
+          {!isLoading && (
+            <div className='main'>
+              <div style={{ fontFamily: '"Noto Sans KR", sans-serif' }} className='geoSelect'>지역을 선택해주세요</div>
+              <div style={{ fontFamily: '"Noto Sans KR", sans-serif' }} className='geoRecommendSelect'>추천받고 싶은 지역을 선택하세요</div>
+              <div style={{ fontFamily: '"Noto Sans KR", sans-serif' }} className='geoRecommendSelect'>위도:{`${nowLocation.latitude}`} 경도 : {`${nowLocation.longitude}`}</div>              
+              <div className='mainPanel'>
+                <RegionButton></RegionButton>
+              </div>
+            </div>
+          )}
+          <Footer hasFooter={true} />
+        </Layout>
+      </div>
     );
   }
 }
