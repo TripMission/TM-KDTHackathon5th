de45e0303c577632e832803e7ad14f32dd70823a
kdg <wlsdk1095sd@gmail.com>
2023-11-07 16:32:35 +0900

    fix: 활동 카드 수정

--- FrontEnd/TMproject/src/components/ActivityCard.jsx
+++ FrontEnd/TMproject/src/components/ActivityCard.jsx
@@ -1,25 +1,32 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import styled from "styled-components";
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import styled from 'styled-components';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ActivityCard({ src, title, content }) {
-  return (
-    <Card style={{ width: "100%" }}>
-      {/* <Card.Img style={{ objectFit: "cover" }} variant="top" src={src} /> */}
-      <StyledImg src={src}></StyledImg>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{content}</Card.Text>
-        <br />
-        <Button variant="primary">{"도전하기"}</Button>
-      </Card.Body>
-    </Card>
-  );
+    const navigate = useNavigate();
+    const nav = () => {
+        navigate('/?'); //도전과제 사진 찍는 페이지로 넘어감
+    };
+    return (
+        <Card style={{ width: '100%' }}>
+            {/* <Card.Img style={{ objectFit: "cover" }} variant="top" src={src} /> */}
+            <StyledImg src={src}></StyledImg>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{content}</Card.Text>
+                <br />
+                <Button variant="danger" onClick={nav}>
+                    {'도전하기'}
+                </Button>
+            </Card.Body>
+        </Card>
+    );
 }
 
 const StyledImg = styled.img`
-  width: 100%;
-  aspect-ratio: 3 / 2;
-  object-fit: contain;
+    width: 100%;
+    aspect-ratio: 3 / 2;
+    object-fit: contain;
 `;
--- FrontEnd/TMproject/src/components/Footer.jsx
+++ FrontEnd/TMproject/src/components/Footer.jsx
@@ -1,83 +1,80 @@
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import { THEME } from "../constants/colors";
-import { HomeIcon, ActivityIcon, ProfileIcon } from "./Icons";
-import usePath from "../hooks/usePath";
-import useLoading from "../hooks/useLoading";
-import React from 'react'
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { THEME } from '../constants/colors';
+import { HomeIcon, ActivityIcon, ProfileIcon } from './Icons';
+import usePath from '../hooks/usePath';
+import useLoading from '../hooks/useLoading';
+import React from 'react';
 
 export default function Footer({ hasFooter }) {
-  const navigate = useNavigate();
-  const { getNthPath } = usePath();
-  const { load, endLoad } = useLoading();
+    const navigate = useNavigate();
+    const { getNthPath } = usePath();
+    const { load, endLoad } = useLoading();
 
-  if (!hasFooter) return null;
+    if (!hasFooter) return null;
 
-  return (
-    <Main>
-      <FooterItem
-        onClick={() => navigate("/Main")}
-        selected={getNthPath(1) === "Main"}
-      >
-        <HomeIcon />
-        메인
-      </FooterItem>
-      <FooterItem
-        onClick={() => {
-          load();
-          setTimeout(() => {
-            endLoad();
-            navigate("/SubPage");
-          }, Math.random() * 300 + 300);
-        }}
-        selected={getNthPath(1) === "SubPage"}
-      >
-        <ActivityIcon />
-        지역
-      </FooterItem>
-      <FooterItem
-        onClick={() => {
-          load();
-          setTimeout(() => {
-            endLoad();
-            navigate("/MyPage");
-          }, Math.random() * 300 + 300);
-        }}
-        selected={getNthPath(1) === "MyPage"}
-      >
-        <ProfileIcon />
-        마이홈
-      </FooterItem>
-    </Main>
-  );
+    return (
+        <Main>
+            <FooterItem onClick={() => navigate('/Main')} selected={getNthPath(1) === 'Main'}>
+                <HomeIcon />
+                메인
+            </FooterItem>
+            <FooterItem
+                onClick={() => {
+                    load();
+                    setTimeout(() => {
+                        endLoad();
+                        navigate('/SubPage');
+                    }, Math.random() * 300 + 300);
+                }}
+                selected={getNthPath(1) === 'SubPage'}
+            >
+                <ActivityIcon />
+                지역
+            </FooterItem>
+            <FooterItem
+                onClick={() => {
+                    load();
+                    setTimeout(() => {
+                        endLoad();
+                        navigate('/MyPage');
+                    }, Math.random() * 300 + 300);
+                }}
+                selected={getNthPath(1) === 'MyPage'}
+            >
+                <ProfileIcon />
+                마이홈
+            </FooterItem>
+        </Main>
+    );
 }
 
 const Main = styled.footer`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
 
-  height: 5rem;
+    height: 5rem;
 
-  position: fixed;
-  z-index: 100000;
-  left: 0;
-  right: 0;
-  bottom: 0px;
-  background-color: white;
-  border-radius: 10px 10px 0 0;
-  box-shadow: 0 -4px 36px rgba(0, 0, 0, 0.07);
+    position: fixed;
+    z-index: 100000;
+    left: 0;
+    right: 0;
+    bottom: 0px;
+    background-color: white;
+    border-radius: 10px 10px 0 0;
+    box-shadow: 0 -4px 36px rgba(0, 0, 0, 0.07);
 `;
 
 const FooterItem = styled.button`
-  text-align: center;
-  border: none;
-  background: none;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
-  height: 3rem;
-  gap: 4px;
-  font-size: 0.9rem;
-  color: ${(p) => (p.selected ? THEME.darker : THEME.black400)};
+    text-align: center;
+    border: none;
+    background: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    height: 3rem;
+    gap: 4px;
+    font-size: 0.9rem;
+    color: ${(p) => (p.selected ? THEME.darker : THEME.black400)};
 `;
